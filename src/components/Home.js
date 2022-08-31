48357d2c525e48ddc80538b1f14de68058ed4fc1
soner <kucukilsoner@gmail.com>
2022-08-31 15:19:09 +0300

    USED TO CLASS COMP

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Home = () => {
+    return (
+        <div className='ui raised very padded text container segment' style={{marginTop:'80px'}}> 
+            <h3 className='ui header'>Home
+            </h3>
+            <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi nisi eius dolore perferendis alias eos inventore laborum repellat nostrum qui ad architecto, aliquid numquam reiciendis nam quis voluptatem at maiores praesentium eligendi velit? Voluptatum sunt eum, molestiae iure corporis corrupti optio odio in dignissimos! Consequatur quia impedit saepe optio obcaecati accusantium magnam suscipit ex quos dolore fugit non consequuntur molestiae voluptas labore, itaque, nemo eos ratione nulla ad at, illum mollitia dolor? Hic placeat laborum praesentium, illum ipsam at distinctio, ad veniam harum odio, aut non earum iusto architecto. Veniam numquam assumenda molestiae maiores necessitatibus itaque, enim mollitia adipisci non.
+            </p>
+        </div>
+    )
+}
+
+export default Home;
